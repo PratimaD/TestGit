8bf9cc864a2382f99350bfa55595576d3c0fd5b0
PratimaD <pratima.dolli@gmail.com>
2017-08-19 13:04:28 +0530

    second commit

--- GitTestApplication/src/main/java/com/exa/test/Test.java
+++ GitTestApplication/src/main/java/com/exa/test/Test.java
@@ -2,8 +2,6 @@ package com.exa.test;
 
 public class Test {
 public static void main(String[] args) {
-System.out.println("Hello World!!!!!!!!!!!!!!");	
-
-
+System.out.println("Hello World!!!!!!!!!!!!!!");
 }
 }
